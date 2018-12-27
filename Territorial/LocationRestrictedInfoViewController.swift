c0b0375e99b56cd0d555836da1c0000a88adb385
Zoreslav Khimich <z.khymych@modnakasta.ua>
2018-12-28 00:19:20 +0200

    Restricted location access view controller.

--- Territorial/LocationRestrictedInfoViewController.swift
+++ Territorial/LocationRestrictedInfoViewController.swift
@@ -9,12 +9,42 @@
 import UIKit
 
 class LocationRestrictedInfoViewController: UIViewController {
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        view.backgroundColor = .red
+    
+    @objc func openSettings() {
+        guard let settingsURL = URL(string: UIApplication.openSettingsURLString) else { return }
+        UIApplication.shared.open(settingsURL, options: [:], completionHandler: nil)
+    }
+    
+    override func loadView() {
+        super.loadView()
+        let contentView = makePromptView()
+        view.addSubview(contentView)
+        contentView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+    }
+    
+    func makePromptView() -> UIView {
+        
+        let promptView = PromptView(frame: view.frame)
+        
+        promptView.picture.image = UIImage(named: "LocationRestrictedPicture")
+        
+        promptView.message.text = NSLocalizedString("Frankly, this app can't do anything without Location access. Could you please use Settings to fix this?", comment: "Location unavailable info message.")
+        promptView.button.setTitle(NSLocalizedString("Open Settings", comment: "Location unavailable Open Settings button title."), for: .normal)
+        promptView.button.addTarget(self, action: #selector(openSettings), for: .touchUpInside)
+        
+        return promptView
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
     }
+
+    init() {
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("Not supported")
+    }
+
 }
